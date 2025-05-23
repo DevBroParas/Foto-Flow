4843af97813b0cd08b154075786450693c768717
DevBroParas <parasprofessionaly@gmail.com>
2025-05-24 01:55:36 +0530

    added cookie-parser

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,26 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
-app.use(cors(
-    {
-        origin: "http://localhost:3000",
-        credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 import userRoute from "./Routes/user.route";
 app.use("/user", userRoute);
 
-
-
-
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
