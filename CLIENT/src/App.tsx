be1528332597a9f431cd600c08380552033e2f42
DevBroParas <parasprofessionaly@gmail.com>
2025-06-01 16:56:13 +0530

    Frontend initial comment

---
+++ CLIENT/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-700">
+      <h1 className="text-3xl font-bold ">Welcome to the React App!</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
