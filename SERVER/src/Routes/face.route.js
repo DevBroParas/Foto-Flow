1c642fcfb41c7a215d6501476cf2f2f860721213
DevBroParas <parasprofessionaly@gmail.com>
2025-07-14 17:46:55 +0530

    deleted js files

--- SERVER/src/Routes/face.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const face_controller_1 = require("../Controller/face.controller");
-const auth_middleware_1 = require("../Middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.post("/faces/:faceId/confirm", auth_middleware_1.protect, face_controller_1.updateFace);
-exports.default = router;
